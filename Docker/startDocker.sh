5d9e2c13fd90d312e7277e890db11779be27b1e7
codesteller <pallab.vsp@gmail.com>
2021-05-31 21:54:20 +0530

    Parameterized docker run

--- Docker/startDocker.sh
+++ Docker/startDocker.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-if [ $1 == all ] 
-then 
-    docker run --gpus all -it --rm -p 9000:8888 -p 0.0.0.0:9006:6000 --shm-size=1g --ulimit memlock=-1 -v $(pwd)/..:/workspace/myspace -w /workspace/myspace  nvcr.io/nvidia/tensorflow:21.05-tf2-py3
-else 
-    docker run --gpus '"device=$1"' -it --rm -p 9000:8888 -p 0.0.0.0:9006:6000 --shm-size=1g --ulimit memlock=-1 -v $(pwd)/..:/workspace/myspace -w /workspace/myspace nvcr.io/nvidia/tensorflow:21.05-tf2-py3
-fi 
+if [ "$#" -ne 2 ]; then
+    echo "Illegal number of parameters." 
+    echo "Usage - ./startDocker.sh <GPU_ID> <Workspace_Path>"
+    echo "Example: ./startDocker.sh 7 $(pwd)/.."
+    exit 1
+fi
 
-# docker run --gpus '"device=$1"' -it --rm -p 9000:8888 -p 0.0.0.0:9006:6000 --shm-size=1g --ulimit memlock=-1 -v $(pwd)/..:/workspace/myspace -w /workspace/myspace  nvcr.io/nvidia/tensorflow:21.05-tf2-py3
+gpu_id=$1
+gpu_device="device=${gpu_id}"
+echo $gpu_device
+wspath=$2
+echo $wspath
+
+if [ $1 != all ]; then 
+    docker run --gpus device=$1 -it --rm -p 9000:8888 -p 0.0.0.0:9006:6000 --shm-size=1g --ulimit memlock=-1 -v $2:/workspace/myspace -w /workspace/myspace  nvcr.io/nvidia/tensorflow:21.05-tf2-py3
+else 
+    docker run --gpus all -it --rm -p 9000:8888 -p 0.0.0.0:9006:6000 --shm-size=1g --ulimit memlock=-1 -v $2:/workspace/myspace -w /workspace/myspace nvcr.io/nvidia/tensorflow:21.05-tf2-py3
+fi 
